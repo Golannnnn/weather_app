0b9eceab25d72334010fd89733728ff50ab1e7ce
Golannnnn <forburacc@protonmail.ch>
2022-10-09 11:17:27 +0300

    add generator line

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext][query]',
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext][query]',
+        },
       },
     ],
   },
