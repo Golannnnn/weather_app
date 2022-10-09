75065624527b28ff5447b2f1cbd4a40e5117b0e0
Golannnnn <forburacc@protonmail.ch>
2022-10-09 09:33:47 +0300

    add function to fetch api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
-// npm start to run dev-server
+
+const fetchCity = async (city) => {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1f50d61745efcac34db5695da7c0205a&units=metric`);
+    if (!response.ok) {
+      if (response.status === 404) {
+        alert('City not found.');
+      }
+      throw new Error(response.status);
+    }
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.log(err);
+  }
+};
